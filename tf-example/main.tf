d8c5ad0bf3f81b9c167bbc93532c9064ca72ea77
JazC <jzchan7713@gmail.com>
2022-11-26 10:05:02 -0800

    changed key pair

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -21,7 +21,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  key_name      = "app-ssh-key"
+  key_name      = "A00902160uswest1"
 
   tags = {
     Name = var.ec2_name
